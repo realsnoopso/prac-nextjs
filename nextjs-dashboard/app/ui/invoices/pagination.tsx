d2b774a8ff9e874c72500bcae44b97fa9201ff9a
realsnoopso <real@snoop.so>
2023-11-26 00:03:47 +0900

    Update href paths in Next.js dashboard components

--- nextjs-dashboard/app/ui/invoices/pagination.tsx
+++ nextjs-dashboard/app/ui/invoices/pagination.tsx
@@ -77,7 +77,7 @@ function PaginationNumber({
   return isActive || position === 'middle' ? (
     <div className={className}>{page}</div>
   ) : (
-    <Link href={href} className={className}>
+    <Link href={{ pathname: href }} className={className}>
       {page}
     </Link>
   );
@@ -112,7 +112,7 @@ function PaginationArrow({
   return isDisabled ? (
     <div className={className}>{icon}</div>
   ) : (
-    <Link className={className} href={href}>
+    <Link className={className} href={{ pathname: href }}>
       {icon}
     </Link>
   );
